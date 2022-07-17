51a52350731506bd34475e6069933582fb18a197
Connor Younglund <cdy@airmail.cc>
2022-07-17 03:18:28 -0400

    use enum for primitive mode

--- src/main/kotlin/models/glTF/Primitive.kt
+++ src/main/kotlin/models/glTF/Primitive.kt
@@ -4,5 +4,5 @@ import com.fasterxml.jackson.annotation.JsonInclude
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 class Primitive(val attributes: Attributes, val material: Int?) {
-    val mode = 4
+    val mode = PrimitiveMode.TRIANGLES.value
 }
