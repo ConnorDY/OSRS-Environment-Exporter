a7463fadc0521c72c18437caa62742713d394084
Connor Younglund <cdy@airmail.cc>
2022-07-16 22:14:43 -0400

    remove unused code

--- src/main/kotlin/models/glTF/Material.kt
+++ src/main/kotlin/models/glTF/Material.kt
@@ -3,39 +3,17 @@ package models.glTF
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonInclude.Include
 
-class Material {
+class Material (texture: Int) {
     @JsonInclude(Include.NON_NULL)
-    class PbrMetallicRoughness() {
+    class PbrMetallicRoughness(texture: Int) {
         class BaseColorTexture (val index: Int)
 
-        var metallicFactor: Float? = null
-        var roughnessFactor: Float? = null
-
-        var baseColorFactor: Array<Float>? = null
-        var baseColorTexture: BaseColorTexture? = null
-
-        constructor(color: Array<Float>): this() {
-            this.baseColorFactor = color
-            this.metallicFactor = 0.0f
-            this.roughnessFactor = 1.0f
-        }
-
-        constructor(texture: Int): this() {
-            this.baseColorTexture = BaseColorTexture(texture)
-        }
+        val baseColorTexture = BaseColorTexture(texture)
     }
 
-    var pbrMetallicRoughness: PbrMetallicRoughness
+    val pbrMetallicRoughness = PbrMetallicRoughness(texture)
 
     val doubleSided = true
     val alphaMode = "MASK"
     val alphaCutOff = 0.0f
-
-    constructor(color: Array<Float>) {
-       this.pbrMetallicRoughness = PbrMetallicRoughness(color)
-    }
-
-    constructor(texture: Int) {
-        this.pbrMetallicRoughness = PbrMetallicRoughness(texture)
-    }
 }
