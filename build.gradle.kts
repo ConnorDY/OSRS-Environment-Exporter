bf45e42365bbaab66f1d8306223e444455cf6d20
Score_Under <seejay.11@gmail.com>
2022-07-21 07:57:15 +0100

    Use split jogamp packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,19 +9,58 @@ group = "org.example"
 version = "2.0.0"
 
 repositories {
-    mavenCentral()
+    mavenCentral {
+        content {
+            excludeGroup("org.jogamp.jogl")
+        }
+    }
+    ivy {
+        // url = uri("https://jogamp.org/deployment/")
+        url = uri("https://score.moe/m/jogamp.org/deployment/")
+        patternLayout {
+            artifact("v[revision]/jar/[module].[ext]")
+        }
+        content {
+            includeGroup("org.jogamp.jogl")
+        }
+        metadataSources {
+            artifact()
+        }
+    }
 }
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("com.displee:rs-cache-library:6.8.1")
     implementation("org.bitbucket.akornilov.kotlin:binary-streams:0.33")
-    implementation(files("lib/jogamp-fat.jar"))
     implementation("org.jsoup:jsoup:1.14.3")
     implementation("org.apache.commons:commons-compress:1.21")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.13.3")
     implementation("ch.qos.logback:logback-classic:1.2.11")
     implementation("org.slf4j:slf4j-api:1.7.36")
+
+    // Jogamp and dependencies thereof
+    implementation("org.jogamp.jogl:gluegen:2.4.0-rc-20210111")
+    implementation("org.jogamp.jogl:jogl-all:2.4.0-rc-20210111")
+    for (
+        p in listOf(
+            "android-aarch64",
+            "android-armv6",
+            "android-x86",
+            "ios-amd64",
+            "ios-arm64",
+            "linux-aarch64",
+            "linux-amd64",
+            "linux-armv6hf",
+            "linux-i586",
+            "macosx-universal",
+            "windows-amd64",
+            "windows-i586"
+        )
+    ) {
+        implementation("org.jogamp.jogl:gluegen-rt-natives-$p:2.4.0-rc-20210111")
+        implementation("org.jogamp.jogl:jogl-all-natives-$p:2.4.0-rc-20210111")
+    }
 }
 
 tasks {
