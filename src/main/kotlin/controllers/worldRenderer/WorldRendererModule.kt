63f51920c57b29a0e1cb79ee4304df13d976c389
Score_Under <seejay.11@gmail.com>
2022-07-21 02:42:04 +0100

    Request single renderer, rather than making renderer singleton

--- src/main/kotlin/controllers/worldRenderer/WorldRendererModule.kt
+++ src/main/kotlin/controllers/worldRenderer/WorldRendererModule.kt
@@ -1,9 +1,28 @@
 package controllers.worldRenderer
 
 import com.google.inject.AbstractModule
+import com.google.inject.Inject
+import com.google.inject.Provider
+import com.google.inject.Singleton
+import models.DebugModel
+import models.scene.Scene
 
 class WorldRendererModule : AbstractModule() {
     override fun configure() {
         bind(Camera::class.java).toInstance(Camera())
+        bind(Renderer::class.java).toProvider(RendererProvider::class.java)
     }
+}
+
+@Singleton
+class RendererProvider @Inject constructor(
+    camera: Camera,
+    scene: Scene,
+    sceneUploader: SceneUploader,
+    inputHandler: InputHandler,
+    textureManager: TextureManager,
+    debugModel: DebugModel
+) : Provider<Renderer> {
+    private val renderer = Renderer(camera, scene, sceneUploader, inputHandler, textureManager, debugModel)
+    override fun get() = renderer
 }
