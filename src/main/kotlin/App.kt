51e363a30ada00d7b50611010277b0f6f678d763
Score_Under <seejay.11@gmail.com>
2022-07-30 14:09:48 +0100

    Enable antialiasing by default unless overridden on command line

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -7,7 +7,16 @@ import javax.swing.ToolTipManager
 import javax.swing.UIManager
 import javax.swing.UnsupportedLookAndFeelException
 
+private fun setSysProperty(key: String, value: String) {
+    if (System.getProperty(key) == null) {
+        System.setProperty(key, value)
+    }
+}
+
 fun main() {
+    setSysProperty("awt.useSystemAAFontSettings", "on")
+    setSysProperty("swing.aatext", "true")
+
     SwingUtilities.invokeLater {
         try {
             UIManager.setLookAndFeel(RadianceGraphiteAquaLookAndFeel())
