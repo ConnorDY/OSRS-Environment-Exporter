ed1450ca326d78712fc64259d502405886f37dd0
Connor Younglund <cdy@airmail.cc>
2022-08-04 17:05:21 -0400

    replace locations.json with a more complete data set

--- src/main/kotlin/models/locations/Location.kt
+++ src/main/kotlin/models/locations/Location.kt
@@ -6,5 +6,4 @@ import com.fasterxml.jackson.annotation.JsonProperty
 data class Location @JsonCreator constructor(
     @JsonProperty("name") val name: String,
     @JsonProperty("coords") val coords: Array<Int>,
-    @JsonProperty("size") val size: String?
 )
