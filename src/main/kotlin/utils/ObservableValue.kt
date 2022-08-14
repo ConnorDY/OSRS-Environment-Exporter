25d0d798ef03a93fad8ba380751b9aa1d476418c
Score_Under <seejay.11@gmail.com>
2022-08-14 20:35:48 +0100

    Use Consumer in action listeners; add contravariance

--- src/main/kotlin/utils/ObservableValue.kt
+++ src/main/kotlin/utils/ObservableValue.kt
@@ -1,24 +1,26 @@
 package utils
 
+import java.util.function.Consumer
+
 class ObservableValue<T>(initial: T) {
     private var backing = initial
-    private val earlyListeners = ArrayList<(T) -> Unit>()
-    private val listeners = ArrayList<(T) -> Unit>()
+    private val earlyListeners = ArrayList<Consumer<in T>>()
+    private val listeners = ArrayList<Consumer<in T>>()
 
     fun get() = backing
     fun set(value: T) {
         if (backing !== value) {
             backing = value
-            earlyListeners.forEach { it.invoke(value) }
-            listeners.forEach { it.invoke(value) }
+            earlyListeners.forEach { it.accept(value) }
+            listeners.forEach { it.accept(value) }
         }
     }
 
-    fun addEarlyListener(listener: (T) -> Unit) {
+    fun addEarlyListener(listener: Consumer<in T>) {
         earlyListeners.add(listener)
     }
 
-    fun addListener(listener: (T) -> Unit) {
+    fun addListener(listener: Consumer<in T>) {
         listeners.add(listener)
     }
 }
