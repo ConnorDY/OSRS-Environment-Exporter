4e0a026df03d44aeb0ffd602f8d9cd50f7af2987
Score_Under <seejay.11@gmail.com>
2022-08-15 11:45:56 +0100

    Remove now-unnecessary add-opens parameters

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 @echo off
-java --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.desktop/sun.awt=ALL-UNNAMED -jar ./build/libs/osrs-environment-exporter-fat-2.1.0.jar
+java -jar ./build/libs/osrs-environment-exporter-fat-2.1.0.jar
