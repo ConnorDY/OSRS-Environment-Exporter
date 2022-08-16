2481bb3d55e195d3e8ab573b0a9c5f98dae84b23
Score_Under <seejay.11@gmail.com>
2022-08-16 02:40:51 +0100

    Add ordered dither alpha algorithm

--- src/main/kotlin/controllers/worldRenderer/helpers/AlphaMode.kt
+++ src/main/kotlin/controllers/worldRenderer/helpers/AlphaMode.kt
@@ -4,5 +4,6 @@ enum class AlphaMode(val internalId: Int, val humanReadableName: String) {
     BLEND(0, "Blend"),
     CLIP(1, "Clip at 0.1"),
     HASH(2, "Hash"),
+    ORDERED_DITHER(4, "Ordered Dither"),
     IGNORE(3, "Opaque")
 }
