cb2ebfbc53e634c587a54b99a32360832c3ee0f7
Score_Under <seejay.11@gmail.com>
2022-08-17 04:30:09 +0100

    Newly discovered location types

--- src/main/kotlin/cache/LocationType.kt
+++ src/main/kotlin/cache/LocationType.kt
@@ -32,6 +32,21 @@ enum class LocationType(val id: Int) {
     INTERACTABLE_WALL(5),
 
     /**
+     * A decoration, such as a torch, on the outside of a wall.
+     */
+    OUTSIDE_WALL_DECORATION(6),
+
+    /**
+     * A decoration, such as a torch, on the inside of a wall.
+     */
+    INSIDE_WALL_DECORATION(7),
+
+    /**
+     * An object placed on a diagonal wall that can be interacted with by a player.
+     */
+    DIAGONAL_INTERACTABLE_WALL_DECORATION(8),
+
+    /**
      * A wall joint that is presented diagonally with respect to the tile.
      */
     DIAGONAL_WALL(9),
@@ -45,6 +60,58 @@ enum class LocationType(val id: Int) {
      * An [INTERACTABLE] object, rotated `pi / 2` radians.
      */
     DIAGONAL_INTERACTABLE(11),
+
+    /**
+     * A roof tile with a vertical slope.
+     */
+    SLOPED_ROOF(12),
+
+    /**
+     * As [SLOPED_ROOF], but a diagonal outer corner.
+     */
+    SLOPED_ROOF_OUTER_CORNER(13),
+
+    /**
+     * As [SLOPED_ROOF], but a diagonal inner corner.
+     * i.e. concave roof shapes, or tessellation with the other corner tile
+     * to create diagonal roofs.
+     */
+    SLOPED_ROOF_INNER_CORNER(14),
+
+    /**
+     * As [SLOPED_ROOF_INNER_CORNER], but with a seam running vertically.
+     */
+    SLOPED_ROOF_HARD_INNER_CORNER(15),
+
+    /**
+     * As [SLOPED_ROOF_OUTER_CORNER], but with a seam running vertically.
+     */
+    SLOPED_ROOF_HARD_OUTER_CORNER(16),
+
+    /**
+     * A flat rooftop tile.
+     */
+    FLAT_ROOF(17),
+
+    /**
+     * A sloped overhanging roof tile.
+     */
+    SLOPED_ROOF_OVERHANG(18),
+
+    /**
+     * As [SLOPED_ROOF_OVERHANG], but a diagonal outer corner.
+     */
+    SLOPED_ROOF_OVERHANG_OUTER_CORNER(19),
+
+    /**
+     * As [SLOPED_ROOF_OVERHANG], but a diagonal inner corner.
+     */
+    SLOPED_ROOF_OVERHANG_INNER_CORNER(20),
+
+    /**
+     * As [SLOPED_ROOF_OVERHANG_OUTER_CORNER], but with a seam running vertically.
+     */
+    SLOPED_ROOF_OVERHANG_HARD_OUTER_CORNER(21),
 
     /**
      * A decoration positioned on the floor.
