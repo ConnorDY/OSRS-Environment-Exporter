7957ba1fe481c7d41f12a807c40982040996ad7b
Score_Under <seejay.11@gmail.com>
2022-08-21 05:19:00 +0100

    Remove close button from progress dialog
    It doesn't do anything useful, just hides the dialog. Its purpose would
    be quite ambiguous; should it hide or cancel?

--- src/main/kotlin/ui/ProgressDialog.kt
+++ src/main/kotlin/ui/ProgressDialog.kt
@@ -1,11 +1,16 @@
 package ui
 
+import org.pushingpixels.radiance.theming.internal.ui.RadianceButtonUI
 import utils.ProgressContainer
+import java.awt.Component
+import java.awt.Container
 import java.awt.Frame
 import java.awt.GridBagConstraints
 import java.awt.GridBagLayout
 import java.awt.Insets
+import java.util.LinkedList
 import javax.swing.JButton
+import javax.swing.JComponent
 import javax.swing.JDialog
 import javax.swing.JLabel
 import javax.swing.JProgressBar
@@ -37,9 +42,8 @@ class ProgressDialog(owner: Frame?, title: String, message: String) : JDialog(ow
         defaultCloseOperation = DISPOSE_ON_CLOSE
         layout = GridBagLayout()
         isUndecorated = true
-//        setDefaultLookAndFeelDecorated(true)
         rootPane.windowDecorationStyle = JRootPane.PLAIN_DIALOG
-        rootPane.windowDecorationStyle = JRootPane.INFORMATION_DIALOG
+        removeCloseButton()
 
         val mainLabel = JLabel(message)
         progressBar = JProgressBar(0, 100).also {
@@ -89,6 +93,31 @@ class ProgressDialog(owner: Frame?, title: String, message: String) : JDialog(ow
         }
 
         pack()
+    }
+
+    private fun removeCloseButton() {
+        rootPane.findSubComponent {
+            it is JComponent && it.getClientProperty(RadianceButtonUI.IS_TITLE_CLOSE_BUTTON) == true
+        }?.isVisible = false
+    }
+
+    private fun Container.findSubComponent(action: (Component) -> Boolean): Component? {
+        val queue = LinkedList<Component>()
+        queue.add(this)
+
+        while (queue.isNotEmpty()) {
+            val component = queue.poll()
+
+            if (action(component)) {
+                return component
+            }
+
+            if (component is Container) {
+                queue.addAll(component.components)
+            }
+        }
+
+        return null
     }
 
     override fun complete() {
