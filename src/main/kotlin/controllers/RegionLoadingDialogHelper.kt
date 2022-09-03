cf475dd320d3bd0d1f70124bbc248b7c142a03e3
Score_Under <seejay.11@gmail.com>
2022-09-03 23:07:52 +0100

    Make region loading warning appear for slightly smaller radiuses too
    These are still a problem for the previewer

--- src/main/kotlin/controllers/RegionLoadingDialogHelper.kt
+++ src/main/kotlin/controllers/RegionLoadingDialogHelper.kt
@@ -15,7 +15,7 @@ object RegionLoadingDialogHelper {
     }
 
     fun confirmRegionLoad(parent: Component, regions: Int): Boolean {
-        if (regions < 400) return true
+        if (regions < 150) return true
         val message = "Are you sure you want to load $regions regions?\n" +
             "This will use a lot of memory. For populated regions, expect 25MB of VRAM per region.\n" +
             "This gives a ballpark estimate of ${(regions * 25 / 1024.0).roundToInt()}GB peak memory usage / VRAM usage.\n" +
