e6488f104d64469acc4f908e0755d8f8573b89d7
Score_Under <seejay.11@gmail.com>
2022-09-04 03:57:12 +0100

    Use progress listeners in CLI

--- src/main/kotlin/environmentexporter/CliExporter.kt
+++ src/main/kotlin/environmentexporter/CliExporter.kt
@@ -11,11 +11,14 @@ import cache.loaders.SpriteLoader
 import cache.loaders.TextureLoader
 import cache.loaders.UnderlayLoader
 import com.displee.cache.CacheLibrary
+import controllers.main.CountingSceneLoadProgressListener
 import controllers.worldRenderer.SceneExporter
 import controllers.worldRenderer.TextureManager
 import models.DebugOptionsModel
 import models.scene.Scene
 import models.scene.SceneRegionBuilder
+import utils.ChunkWriteListener
+import utils.ProgressContainer
 
 class CliExporter(startupOptions: StartupOptions) {
     val scene: Scene
@@ -47,10 +50,52 @@ class CliExporter(startupOptions: StartupOptions) {
             // Export the scene once it has been loaded.
             exporter.exportSceneToFile(scene, startupOptions.exportDir, startupOptions.exportFlat)
         }
-        // TODO: progress indicator hooks
+
+        // Listen for progress updates
+        scene.sceneLoadProgressListeners.add(object : CountingSceneLoadProgressListener() {
+            override val progressContainer: ProgressContainer get() = CliProgressContainer
+            override val statusDoing: String get() = "Loading region $currentRegion of $numRegions"
+            override val statusDone: String get() = "Loaded regions"
+        })
+        exporter.sceneLoadProgressListeners.add(object : CountingSceneLoadProgressListener() {
+            override val progressContainer: ProgressContainer get() = CliProgressContainer
+            override val statusDoing: String get() = "Exporting region $currentRegion of $numRegions"
+            override val statusDone: String get() = "Exported regions"
+        })
+        exporter.chunkWriteListeners.add(object : ChunkWriteListener {
+            override fun onStartRegion(regionNum: Int) {
+                if (regionNum == -1)
+                    println("Writing JSON")
+                else
+                    println("Writing region $regionNum")
+            }
+
+            override fun onEndRegion() {}
+
+            override fun onFinishWriting() {
+                println("Done!")
+            }
+        })
     }
 
     fun exportRadius(regionId: Int, radius: Int) {
         scene.loadRadius(regionId, radius)
+    }
+
+    object CliProgressContainer : ProgressContainer {
+        override var progress: Int = 0
+        override var progressMax: Int = 100
+        override var status: String
+            get() = ""
+            set(value) {
+                println(value)
+            }
+        override var isCancelled: Boolean
+            get() = false
+            set(value) {
+                throw UnsupportedOperationException("Cannot cancel from CLI")
+            }
+
+        override fun complete() {}
     }
 }
