42fd27ec18bd3214f844b858927219de5dbcabbc
Score_Under <seejay.11@gmail.com>
2022-09-28 11:43:01 +0100

    Accessibility: Improve keyboard navigation of links

--- src/main/kotlin/ui/JActionLabel.kt
+++ src/main/kotlin/ui/JActionLabel.kt
@@ -4,15 +4,20 @@ import java.awt.Color
 import java.awt.Cursor
 import java.awt.event.ActionEvent
 import java.awt.event.ActionListener
+import java.awt.event.FocusEvent
+import java.awt.event.FocusListener
 import java.awt.event.KeyAdapter
 import java.awt.event.KeyEvent
 import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
 import java.awt.font.TextAttribute
+import javax.swing.BorderFactory
 import javax.swing.JLabel
 import javax.swing.SwingConstants
+import javax.swing.UIManager
 
 open class JActionLabel(text: String, align: Int = SwingConstants.LEADING) : JLabel(text, align) {
+    private val focusBorderColor get() = UIManager.getColor("Label.foreground") ?: Color.BLACK
     private val actionListeners = mutableListOf<ActionListener>()
 
     init {
@@ -20,6 +25,7 @@ open class JActionLabel(text: String, align: Int = SwingConstants.LEADING) : JLa
         addMouseListener(
             object : MouseAdapter() {
                 override fun mouseClicked(e: MouseEvent?) {
+                    requestFocusInWindow(FocusEvent.Cause.MOUSE_EVENT)
                     if (e?.button == MouseEvent.BUTTON1) {
                         fireClickEvent()
                     }
@@ -35,9 +41,24 @@ open class JActionLabel(text: String, align: Int = SwingConstants.LEADING) : JLa
                 }
             }
         )
+
         font = font.let {
             it.deriveFont(it.attributes + (TextAttribute.UNDERLINE to TextAttribute.UNDERLINE_ON))
         }
+        updateBorder(false)
+
+        addFocusListener(
+            object : FocusListener {
+                override fun focusGained(e: FocusEvent?) {
+                    updateBorder(true)
+                }
+
+                override fun focusLost(e: FocusEvent?) {
+                    updateBorder(false)
+                }
+            }
+        )
+
         foreground = Color.BLUE
         isFocusable = true
     }
@@ -54,6 +75,14 @@ open class JActionLabel(text: String, align: Int = SwingConstants.LEADING) : JLa
         actionListeners.forEach {
             val event = ActionEvent(this, ActionEvent.ACTION_PERFORMED, "click")
             it.actionPerformed(event)
+        }
+    }
+
+    open fun updateBorder(focused: Boolean) {
+        border = if (focused) {
+            BorderFactory.createDashedBorder(focusBorderColor, 1f, 1f, 1f, true)
+        } else {
+            BorderFactory.createEmptyBorder(1, 1, 1, 1)
         }
     }
 }
