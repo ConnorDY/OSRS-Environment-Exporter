62f4e48b8535afcd41b1943fec8f364c64993329
Score_Under <seejay.11@gmail.com>
2023-03-25 12:34:16 +0000

    Do not hardcode 3 as maximum Z layer

--- src/main/kotlin/models/StartupOptions.kt
+++ src/main/kotlin/models/StartupOptions.kt
@@ -1,6 +1,7 @@
 package models
 
 import AppConstants.OUTPUT_DIRECTORY
+import cache.definitions.RegionDefinition.Companion.Z
 import models.config.ConfigOptions
 
 class StartupOptions(configOptions: ConfigOptions) {
@@ -13,5 +14,5 @@ class StartupOptions(configOptions: ConfigOptions) {
     var showPreview = true
     var scaleFactor = configOptions.scaleMode.value.get().scaleFactor
     var hasScaleFactor = false
-    var enabledZLayers = (0..3).toList()
+    var enabledZLayers = (0 until Z).toList()
 }
