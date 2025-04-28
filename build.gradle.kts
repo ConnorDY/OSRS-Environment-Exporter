29ef949cae47ca2a92a739539f6b438ccbd5320a
Connor Younglund <cdy@airmail.cc>
2025-04-27 17:39:04 -0700

    update rs-cache-library to 7.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation("com.displee:rs-cache-library:7.1.0")
+    implementation("com.displee:rs-cache-library:7.2.0")
     implementation("org.jsoup:jsoup:1.14.3")
     implementation("org.apache.commons:commons-compress:1.21")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.13.3")
